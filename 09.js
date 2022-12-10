df2fb5c70fbc13f24f6283df5de9a210398aec55
Mariana Rodrigues <rodriguesmariana1106@gmail.com>
2022-12-10 02:13:22 -0300

    Q09

---
+++ 09.js
@@ -0,0 +1,26 @@
+const participantes = [
+    { nome: "João" },
+    { nome: "Ana" },
+    { nome: "Beatriz" },
+    { nome: "Maria" },
+    { nome: "Ana Clara" },
+    { nome: "Joana" },
+    { nome: "Augusto" },
+    { nome: "Renan" },
+    { nome: "Patricia" },
+    { nome: "Carlos" },
+    { nome: "Renato" },
+    { nome: "José" },
+    { nome: "Roberto" },
+    { nome: "Sara" },
+    { nome: "Junior" },
+    { nome: "Pedro" },
+    { nome: "Vitor" },
+    { nome: "Antonio" },
+];
+
+for (let indice = 0; indice < participantes.lenght; indice++) {
+    if (participantes[indice].nome === 'Carlos') {
+        console.log(`Galera... O Carlos está na posição ${indice + 1}, corre lá!`);
+    }
+}
